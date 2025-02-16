d53a32908dae2c6553f93e4f054a05ddb3d5bd8a
Kutikov Pasha <kutikovpasha@gmail.com>
2025-02-16 21:57:32 +0600

    refactor(build): dont minify output

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
+  build: {
+    minify: false,
+    terserOptions: {
+
+      compress: false,
+      mangle: false,
+    },
+  },
 })
