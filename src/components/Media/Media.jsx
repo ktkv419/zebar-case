cd425023593198a311f5bf6647f44160a15d859f
Kutikov Pasha <kutikovpasha@gmail.com>
2025-03-03 23:28:59 +0600

    refactor(media): remove redundant code

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,72 +1,44 @@
 import React, { act, useEffect } from "react"
 import { useState } from "react"
+import Button from "../Button/Button"
 
 const Media = ({ media, glazewm, host }) => {
     const {
-        allSessions,
         pause,
         next,
         previous,
         play,
+        currentSession,
         // Todo: Make current playing position
         // position,
         // endTime,
     } = media
     const [focusedWindow, setFocusedWindow] = useState()
-    const [currentSession, setCurrentSession] = useState(undefined)
 
     function getMedia() {
         if (currentSession) {
-            return `󰝚 ${[currentSession.artist, currentSession.title].join(" - ")}`
+            return `${[currentSession.artist, currentSession.title].join(" - ")}`
         }
-    }
-
-    const handleStop = () => {
-        setCurrentSession(undefined)
-        pause()
     }
 
     useEffect(() => {
         setFocusedWindow(glazewm.allWindows.find((window) => window.hasFocus))
     }, [glazewm])
 
-    useEffect(() => {
-        const activeSession = allSessions.find((session) => session.isPlaying)
-        setCurrentSession(activeSession)
-    }, [allSessions])
-
-    return currentSession && allSessions?.length > 0 ? (
-        <div className="center">
-            <div className="media__btn-box">
-                <span className="btn" onClick={async () => previous()}>
-                    󰒮
-                </span>
-                <span onClick={async () => handleStop()} className="btn">
-                    {getMedia()}
-                </span>
-
-                <span className="btn" onClick={async () => next()}>
-                    󰒭
-                </span>
-            </div>
-        </div>
-    ) : focusedWindow ? (
-        <div
-            className="center title"
-            onClick={async () => {
-                play()
-            }}
-        >
-            <span className={`btn`}>{focusedWindow.processName}</span>
+    return currentSession && currentSession.isPlaying ? (
+        <div className="center media">
+            <Button onClick={previous}>󰒮</Button>
+            <Button onClick={() => pause()}>
+                <span>󰝚</span>
+                {getMedia()}
+            </Button>
+            <Button onClick={next}>󰒭</Button>
         </div>
     ) : (
-        <div
-            className="center title"
-            onClick={async () => {
-                play()
-            }}
-        >
-            <span className={`btn`}>{host.hostname}</span>
+        <div className="center title">
+            <Button onClick={() => play()}>
+                {focusedWindow ? focusedWindow.processName : host.hostname}
+            </Button>
         </div>
     )
 }
