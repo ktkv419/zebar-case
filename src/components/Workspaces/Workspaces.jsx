a7a7b0ce8a8e8ed70b2914716871e13377e702eb
Kutikov Pasha <kutikovpasha@gmail.com>
2025-03-04 09:30:58 +0600

    refactor(workspaces): add buttons as workspaces

--- src/components/Workspaces/Workspaces.jsx
+++ src/components/Workspaces/Workspaces.jsx
@@ -1,60 +1,25 @@
-import { useEffect, useRef, useState } from "react"
+import { useMemo } from "react"
+import Button from "../Button/Button"
 
 const Workspaces = ({ allWorkspaces, runCommand }) => {
-    const [workspaces, setWorkspaces] = useState()
-    const [currentWorkspace, setCurrentWorkspace] = useState()
-    const selectorRef = useRef(null)
-    const [size, setSize] = useState()
-    const [mainRes, setMainRes] = useState(allWorkspaces[0].width)
-
-    useEffect(() => {
-        if (selectorRef.current) {
-            selectorRef.current.style
-        }
-    }, [selectorRef])
-
-    useEffect(() => {
-        const _currentWorkspaces = [...document.querySelectorAll(".workspace")]
-        setWorkspaces(_currentWorkspaces)
-        setCurrentWorkspace(allWorkspaces.findIndex((tab) => tab.hasFocus))
-        setMainRes(allWorkspaces[0].width)
-    }, [allWorkspaces])
-
-    useEffect(() => {
-        if (selectorRef.current && size?.width) {
-            selectorRef.current.style.left = `${
-                size.offset.left - size.width / 2 - 1
-            }px`
-            selectorRef.current.style.width = `${size.width}px`
-        }
-    }, [size, selectorRef])
-
-    useEffect(() => {
-        if (currentWorkspace >= 0) {
-            setSize({
-                width: workspaces[currentWorkspace].clientWidth,
-                offset: workspaces[currentWorkspace].getBoundingClientRect(),
-            })
-        }
-    }, [currentWorkspace])
+    const mainRes = useMemo(() => allWorkspaces[0].width, [])
 
     return (
         <div className="workspaces">
-            <div className="workspace__selector" ref={selectorRef}></div>
             {allWorkspaces
                 .sort((a, b) => a.name - b.name)
                 .map((workspace) => (
-                    <button
-                        className={`workspace ${workspace.hasFocus && "focused"} ${
-                            workspace.isDisplayed && "displayed"
-                        } ${workspace.width !== mainRes && "workspace--secondary"}`}
+                    <Button
+                        key={workspace.name}
                         onClick={() =>
                             runCommand(`focus --workspace ${workspace.name}`)
                         }
-                        key={workspace.name}
+                        className={`workspace ${workspace.hasFocus && "focused"} ${
+                            workspace.isDisplayed && "displayed"
+                        } ${workspace.width !== mainRes && "workspace--secondary"}`}
                     >
                         {workspace.displayName ?? workspace.name}
-                    </button>
+                    </Button>
                 ))}
         </div>
     )
